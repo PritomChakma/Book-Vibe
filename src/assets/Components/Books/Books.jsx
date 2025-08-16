9db5669b9d91697a0924fff19fd662bad992b9fc
Pritom Chakma <chakmapritom1@gmail.com>
2025-08-17 01:09:37 +0600

    show the books by api

--- src/assets/Components/Books/Books.jsx
+++ src/assets/Components/Books/Books.jsx
@@ -1,28 +1,25 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import BooksCard from "./BooksCard";
 
 const Books = () => {
+  const [books, setBooks] = useState([]);
 
-const [books, setBooks] = useState([])
-
-useEffect(()=>{
+  useEffect(() => {
     fetch("../../../../public/booksData.json")
-    .then(res => res.json())
-    .then(data => setBooks(data))
-},[])
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
 
-    return (
-        <div>
-            <h1 className="text-4xl font-bold text-center">Books</h1>
-<div>
-    {
-        <h1>{books.length}</h1>
-        
-    }
-</div>
-
-        </div>
-    );
+  return (
+    <div >
+      <h1 className="text-4xl font-bold text-center my-10">Books</h1>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {books.map((book) => (
+          <BooksCard key={book.bookId} book={book}></BooksCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Books;
